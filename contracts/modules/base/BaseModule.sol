81f00a0c0ed17fd8089f819d0dc48a4205a24ff1
Ramarti <raul@storyprotocol.xyz>
2023-10-25 16:58:50 -0300

    add configure to module (#135)
    Co-authored-by: Raul <raul@proofofcode.dev>

--- contracts/modules/base/BaseModule.sol
+++ contracts/modules/base/BaseModule.sol
@@ -5,6 +5,7 @@ import { IModule } from "contracts/interfaces/modules/base/IModule.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { FranchiseRegistry } from "contracts/FranchiseRegistry.sol";
 import { HookRegistry } from "./HookRegistry.sol";
+import { Errors } from "contracts/lib/Errors.sol";
 
 abstract contract BaseModule is IModule, HookRegistry {
 
@@ -16,24 +17,40 @@ abstract contract BaseModule is IModule, HookRegistry {
     address public immutable IPA_REGISTRY;
     address public immutable MODULE_REGISTRY;
 
+    modifier onlyModuleRegistry() {
+        // TODO: Enforce this
+        // if (msg.sender != MODULE_REGISTRY) 
+        //    revert Errors.BaseModule_CallerNotModuleRegistry();
+        _;
+    }
+
     constructor(ModuleConstruction memory params) {
         IPA_REGISTRY = params.ipaRegistry;
         MODULE_REGISTRY = params.moduleRegistry;
     }
 
-    function execute(bytes calldata selfParams, bytes[] calldata preHookParams, bytes[] calldata postHookParams) external {
-        // Should we include a request Id?
-        _verifyExecution(msg.sender, selfParams);
+    function execute(
+        address caller,
+        bytes calldata selfParams,
+        bytes[] calldata preHookParams,
+        bytes[] calldata postHookParams
+    ) external onlyModuleRegistry {
+        _verifyExecution(caller, selfParams);
         if (!_executePreHooks(preHookParams)) {
-            emit RequestPending(msg.sender);
+            emit RequestPending(caller);
             return;
         }
         _performAction(selfParams);
         _executePostHooks(postHookParams);
-        emit RequestCompleted(msg.sender);
+        emit RequestCompleted(caller);
+    }
+
+    function configure(bytes calldata params) external onlyModuleRegistry {
+        _configure(msg.sender, params);
     }
 
     function _hookRegistryAdmin() virtual override internal view returns (address);
+    function _configure(address caller, bytes calldata params) virtual internal;
     function _verifyExecution(address caller, bytes calldata selfParams) virtual internal {}
     function _executePreHooks(bytes[] calldata params) virtual internal returns (bool) {}
     function _performAction(bytes calldata params) virtual internal {}
