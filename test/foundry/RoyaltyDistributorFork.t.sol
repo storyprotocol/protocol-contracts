5d205620341792bdc227fcc03d8ff53ac32786be
kingster-will <83567446+kingster-will@users.noreply.github.com>
2023-11-02 10:10:41 -0700

    Logging Mainnet RPC URL (#148)
    * print log of mainnet rpc url
    
    * avoid print secrets in log
    
    * donot log rpc url
    
    * using pull_request event
    
    * trigger test
    
    * remove public rpc
    
    * revert change

--- test/foundry/RoyaltyDistributorFork.t.sol
+++ test/foundry/RoyaltyDistributorFork.t.sol
@@ -6,16 +6,16 @@ import "test/foundry/RoyaltyDistributor.t.sol";
 contract RoyaltyDistributorForkTest is RoyaltyDistributorTest {
     function _getSplitMain() internal virtual override returns(ISplitMain) {
         string memory mainnetRpc;
+        console.log("fork mainnet");
         try vm.envString("MAINNET_RPC_URL") returns (string memory rpcUrl) {
             mainnetRpc = rpcUrl;
+            console.log("Using mainnet RPC in environment variable");
         } catch {
-            mainnetRpc = "https://rpc.ankr.com/eth";
+            fail("Missing environment variable 'MAINNET_RPC_URL'");
         }
-        console.log(mainnetRpc);
         uint256 mainnetFork = vm.createFork(mainnetRpc);
         vm.selectFork(mainnetFork);
         assertEq(vm.activeFork(), mainnetFork);
-        console.log(block.number);
         return ISplitMain(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE);
     }
 }
