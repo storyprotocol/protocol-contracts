50c7be8c83f4fc9176d59b9fd1c3937f28ba518f
Raul <raul@proofofcode.dev>
2023-11-05 17:43:58 -0300

    add module registyr to SPG

--- contracts/StoryProtocol.sol
+++ contracts/StoryProtocol.sol
@@ -5,16 +5,19 @@ import { IIPOrgFactory } from "contracts/interfaces/ip-org/IIPOrgFactory.sol";
 import { IPOrgParams } from "contracts/lib/IPOrgParams.sol";
 import { Errors } from "contracts/lib/Errors.sol";
 import { IPOrgParams } from "contracts/lib/IPOrgParams.sol";
+import { ModuleRegistry } from "contracts/modules/ModuleRegistry.sol";
 
 contract StoryProtocol {
     // TODO: should this be immutable, or should the protocol be able to change factory
     IIPOrgFactory public immutable FACTORY;
+    ModuleRegistry public immutable MODULE_REGISTRY;
 
-    constructor(IIPOrgFactory ipOrgFactory_) {
-        if (address(ipOrgFactory_) == address(0)) {
+    constructor(IIPOrgFactory ipOrgFactory_, ModuleRegistry moduleRegistry_) {
+        if (address(ipOrgFactory_) == address(0) || address(moduleRegistry_) == address(0)) {
             revert Errors.ZeroAddress();
         }
         FACTORY = ipOrgFactory_;
+        MODULE_REGISTRY = moduleRegistry_;
     }
 
     function registerIpOrg(IPOrgParams.RegisterIPOrgParams calldata params) external returns (address) {
