807ce8d37c6c98d8c9f9f6208ec55823d44a64b0
Leeren <leeren@dopamine.xyz>
2023-11-07 01:18:16 -0800

    Adds core authorization and attribution to the GIPR (#157)
    * Adds core authorization and attribution to the GIPR
    
    * Fixes accidental old code merge
    
    * Fixes IPAssetOrg Test

--- contracts/ip-assets/IPAssetOrg.sol
+++ contracts/ip-assets/IPAssetOrg.sol
@@ -2,18 +2,12 @@
 pragma solidity ^0.8.13;
 
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import { IIPAssetOrg } from "contracts/interfaces/ip-assets/IIPAssetOrg.sol";
-import { ICollectModule } from "contracts/interfaces/modules/collect/ICollectModule.sol";
 import { IPAsset } from "contracts/lib/IPAsset.sol";
 import { IPAssetOrgDataManager } from "./storage/IPAssetOrgDataManager.sol";
 import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import { IERC165Upgradeable } from "@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol";
 import { MulticallUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 import { IPAssetRegistry } from "contracts/IPAssetRegistry.sol";
-import { ILicensingModule } from "contracts/interfaces/modules/licensing/ILicensingModule.sol";
-import { Collect } from "contracts/lib/modules/Collect.sol";
 import { Errors } from "contracts/lib/Errors.sol";
-import { Licensing } from "contracts/lib/modules/Licensing.sol";
 
 /// @notice IP Asset Organization
 contract IPAssetOrg is
@@ -22,7 +16,6 @@ contract IPAssetOrg is
     MulticallUpgradeable,
     OwnableUpgradeable
 {
-
     /// @custom:storage-location erc7201:story-protocol.ip-asset-org.storage
     // TODO: Refactor IP asset types to be specified through the IP Asset Registry or one of its modules.
     struct IPAssetOrgStorage {
@@ -33,7 +26,9 @@ contract IPAssetOrg is
     IPAssetRegistry public REGISTRY;
 
     // keccak256(bytes.concat(bytes32(uint256(keccak256("story-protocol.ip-assets-registry.storage")) - 1)))
-    bytes32 private constant _STORAGE_LOCATION = 0x1a0b8fa444ff575656111a4368b8e6a743b70cbf31ffb9ee2c7afe1983f0e378;
+    bytes32 private constant _STORAGE_LOCATION =
+        0x1a0b8fa444ff575656111a4368b8e6a743b70cbf31ffb9ee2c7afe1983f0e378;
+
     string private constant _VERSION = "0.1.0";
 
     // TODO(ramarti): Refactor to configure IP Asset types via registry modules.
@@ -44,17 +39,14 @@ contract IPAssetOrg is
         return _VERSION;
     }
 
-
-    function initialize(IPAsset.InitIPAssetOrgParams memory params_) public initializer {
-
-        // TODO(ramarti) Decouple IPAssetOrg from the RightsManager and make sure to move `__ERC721_init` here.
-        __ERC721_init(params_.name, params_.symbol);
-
+    function initialize(
+        IPAsset.InitIPAssetOrgParams memory params_
+    ) public initializer {
         __Multicall_init();
         __Ownable_init();
+        __ERC721_init(params_.name, params_.symbol);
         // TODO: Weird bug does not allow OZ to specify owner in init...
         _transferOwnership(params_.owner);
-
 
         if (params_.registry == address(0)) revert Errors.ZeroAddress();
         REGISTRY = IPAssetRegistry(params_.registry);
@@ -69,13 +61,34 @@ contract IPAssetOrg is
     /// @return the created IP Asset id (and IP Asset org id).
     /// TODO(ramarti): Refactor licensing configuration to use registry asset ids instead of ip asset org ids.
     /// TODO(leeren): Deprecate returning of internal IP Asset org id once existing dependencies to it are removed.
-    function createIpAsset(IPAsset.CreateIpAssetParams calldata params_) public returns (uint256, uint256) {
-        if (params_.ipAssetType == IPAsset.IPAssetType.UNDEFINED) revert Errors.IPAsset_InvalidType(IPAsset.IPAssetType.UNDEFINED);
+    function createIpAsset(
+        IPAsset.CreateIpAssetParams calldata params_
+    ) public returns (uint256, uint256) {
+        if (params_.ipAssetType == IPAsset.IPAssetType.UNDEFINED) {
+            revert Errors.IPAsset_InvalidType(IPAsset.IPAssetType.UNDEFINED);
+        }
+
+        IPAsset.RegisterIpAssetParams memory registrationParams = IPAsset
+            .RegisterIpAssetParams({
+                name: params_.name,
+                ipAssetType: 0, // TODO(ramarti): Replace with valid licensing type.
+                owner: params_.to,
+                ipOrg: address(this),
+                hash: "",
+                url: "",
+                data: ""
+            });
+
         // TODO: Add module and other relevant configuration for registration.
-        uint256 ipAssetId = REGISTRY.register(msg.sender, address(this));
+        uint256 ipAssetId = REGISTRY.register(registrationParams);
         uint256 ipAssetOrgId = _mintBlock(params_.to, params_.ipAssetType);
-        _writeIPAsset(ipAssetId, ipAssetOrgId, params_.name, params_.description, params_.mediaUrl);
-        IPAssetOrgStorage storage $ = _getIPAssetOrgStorage();
+        _writeIPAsset(
+            ipAssetId,
+            ipAssetOrgId,
+            params_.name,
+            params_.description,
+            params_.mediaUrl
+        );
 
         return (ipAssetId, ipAssetOrgId);
     }
@@ -98,7 +111,9 @@ contract IPAssetOrg is
         IPAsset.IPAssetType ipAssetType_
     ) private returns (uint256) {
         uint256 nextId = currentIdFor(ipAssetType_) + 1;
-        if (nextId > IPAsset._lastId(ipAssetType_)) revert Errors.IPAssetOrg_IdOverBounds();
+        if (nextId > IPAsset._lastId(ipAssetType_)) {
+            revert Errors.IPAssetOrg_IdOverBounds();
+        }
         IPAssetOrgStorage storage $ = _getIPAssetOrgStorage();
         $.idsByType[ipAssetType_] = nextId;
         _safeMint(to_, nextId);
@@ -107,7 +122,9 @@ contract IPAssetOrg is
 
     /// @notice Gets the next id that a newly minted IP Asset corresponds to.
     /// @param ipAssetType_ The type of the IP Asset being queried.
-    function currentIdFor(IPAsset.IPAssetType ipAssetType_) public view returns (uint256) {
+    function currentIdFor(
+        IPAsset.IPAssetType ipAssetType_
+    ) public view returns (uint256) {
         IPAssetOrgStorage storage $ = _getIPAssetOrgStorage();
         uint256 currentId = $.idsByType[ipAssetType_];
         if (currentId == 0) {
