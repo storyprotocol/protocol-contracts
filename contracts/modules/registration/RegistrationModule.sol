f3d78dec3130af457c781b2694b7c0c1690cf062
Ramarti <raul@storyprotocol.xyz>
2023-11-21 17:06:50 -0300

    remove logs and add Multicall to StoryProtocol.sol (#186)
    Co-authored-by: Raul <raul@proofofcode.dev>

--- contracts/modules/registration/RegistrationModule.sol
+++ contracts/modules/registration/RegistrationModule.sol
@@ -15,8 +15,6 @@ import { LibUintArrayMask } from "contracts/lib/LibUintArrayMask.sol";
 import { Errors } from "contracts/lib/Errors.sol";
 import { IPAsset } from "contracts/lib/IPAsset.sol";
 
-import "forge-std/console2.sol";
-
 /// @title Registration Module
 /// @notice Handles registration and transferring of IP assets..
 contract RegistrationModule is BaseModule, IRegistrationModule, AccessControlled {
