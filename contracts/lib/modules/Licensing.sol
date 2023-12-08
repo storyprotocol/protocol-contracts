d75da14a9b5ef32d50b7359b31f7e438cc534cfd
Raul <raul@proofofcode.dev>
2023-12-08 02:42:44 -0800

    licensing lib commented

--- contracts/lib/modules/Licensing.sol
+++ contracts/lib/modules/Licensing.sol
@@ -9,60 +9,92 @@ import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { ShortStrings, ShortString } from "@openzeppelin/contracts/utils/ShortStrings.sol";
 
 /// @title Licensing Module Library
-/// Structs needed by the Licensing Modules and registries
 library Licensing {
     using ShortStrings for *;
+    /// @notice Enum for the status of a license.
     enum LicenseStatus {
+        /// No status has been set.
         Unset,
+        /// The license is active. It can be linked with IPAs.
         Active,
+        /// The license is pending licensor approval. It can't be linked with IPAs until approved
+        /// by the licensor using the LicensingModule (activateLicense)
         PendingLicensorApproval,
+        /// The license has been revoked. It can't be linked with IPAs or sublicensed.
+        /// Sub-licenses will be considered non active.
         Revoked
     }
 
+    /// @notice Defines the types to encode/decode the parameters of a licensing framework.
     enum ParameterType {
+        /// Boolean value
         Bool,
+        /// Number value (18 decimals)
         Number,
+        /// Address value
         Address,
+        /// String value
         String,
+        /// Array of ShortString values
         ShortStringArray,
         // uint256 bitmask representing indexes in choices array. ParamDefinition will have the available choices array.
         MultipleChoice 
     }
 
+    /// @notice Defines the configuration of the licensor for an IP org.
     enum LicensorConfig {
+        /// Null value.
         Unset,
+        /// Licensor is the IP org owner, for all licenses.
         IpOrgOwnerAlways,
+        /// Licensor will be:
+        /// - If parentLicense is provided, the licensee of the parent license.
+        /// - If parentLicense is not provided, the Owner of the linked IPA.
+        /// - If no parentLicense and no linked IPA, the IP org owner.
         Source
     }
 
+    /// @notice Data struct representing a license agreement minted according to an IP Org Licensing Framework
     struct LicenseData {
-        /// License status. // TODO: IPA status should follow
+        /// License status.
         LicenseStatus status;
+        /// If true, other licenses can set this one as parent license, false will prevent it.
         bool derivativesAllowed;
+        /// If true, sublicenses can be created by anyone, and the terms of this license will be
+        /// applied to the sublicenses. If false, sublicenses can only be created by the licensor.
         bool isReciprocal;
+        /// If true, sublicenses will be minted with LicenseStatus.PendingLicensorApproval.
         bool derivativeNeedsApproval;
+        /// Address with permission to revoke the license.
         address revoker;
         /// address granting the license
         address licensor;
         /// address of the ip org that produced the terms
         address ipOrg;
+        /// The id of the license framework used to mint this license
         ShortString frameworkId;
-        /// If the licensee is bound to an IPA, this is the IPA id. 0 otherwise
+        /// If the licensee is linked to an IPA, this is the IPA id. 0 otherwise
         uint256 ipaId;
-        /// The id of the parent license. 0 if this this is tied to the first IPA of an IPOrg
+        /// The id of the parent license, if any. 0 otherwise
         uint256 parentLicenseId;
     }
 
+    /// @notice Parameters to mint a license
     struct LicenseCreation {
+        /// Array of (tag, value) pairs for the parameters, corresponding to the tags in the
+        /// licensing framework.
         ParamValue[] params;
+        /// Parent license id, if any. 0 otherwise.
         uint256 parentLicenseId;
+        /// Linked IPA id, if any. 0 otherwise.
         uint256 ipaId;
     }
 
+    /// @notice Corresponds to a parameter (licensing term) of a licensing framework.
     struct ParamDefinition {
-        /// The parameter id
+        /// The parameter id, used to identify the parameter in the license agreement text.
         ShortString tag;
-        /// The type of the parameter, used to decode the value
+        /// The type of the parameter, used to encode/decode the value
         ParameterType paramType;
         /// Encoded according to paramType, might be empty.
         bytes defaultValue;
@@ -70,37 +102,49 @@ library Licensing {
         bytes availableChoices;
     }
 
+    /// @notice Corresponds to a value for parameter (licensing term) of a licensing framework.
     struct ParamValue {
+        /// The parameter id, used to identify the parameter in the license agreement text.
         ShortString tag;
+        /// Encoded according to paramType, might be empty.
         bytes value;
     }
-
-    struct FrameworkStorage {
-        string textUrl;
-        FixedSet.ShortStringSet paramTags;
-        ParamDefinition[] paramDefs;
-    }
-
+    
+    /// @notice Parameters to set a licensing framework in LicensingFrameworkRepo
     struct SetFramework {
+        /// The id of the licensing framework.
         string id;
+        /// The URL to the license agreement text.
         string textUrl;
+        /// Array of parameter definitions, corresponding to the parameters in
+        /// the license agreement text.
         ParamDefinition[] paramDefs;
     }
 
+    /// @notice Parameters to set a licensing framework in an IP org
     struct LicensingConfig {
+        /// The id of the licensing framework.
         string frameworkId;
+        /// Array of (tag, value) pairs for the parameters, corresponding to the tags in the
+        /// licensing framework.
         ParamValue[] params;
+        /// Enum with the rules to determine the licensor for this IP org's licenses
         LicensorConfig licensor;
     }
 
-    uint256 constant MAX_PARAM_TAGS = 150;
+    /// @notice Maximum number of parameters allowed in a licensing framework.
+    uint256 constant MAX_PARAM_TAGS = 255;
 
     /// Input for IpOrg legal terms configuration in LicensingModule (for now, the only option)
     bytes32 constant LICENSING_FRAMEWORK_CONFIG = keccak256("LICENSING_FRAMEWORK_CONFIG");
+    /// Create license action in LicensingModule
     bytes32 constant CREATE_LICENSE = keccak256("CREATE_LICENSE");
+    /// Activate license action in LicensingModule
     bytes32 constant ACTIVATE_LICENSE = keccak256("ACTIVATE_LICENSE");
+    /// Revoke license action in LicensingModule
     bytes32 constant LINK_LNFT_TO_IPA = keccak256("LINK_LNFT_TO_IPA");
 
+    /// @notice Returns the string representation of a license status.
     function _statusToString(
         LicenseStatus status_
     ) internal pure returns (string memory) {
@@ -116,6 +160,11 @@ library Licensing {
         return "Unknown";
     }
 
+    /// @notice decodes value from a MultipleChoice parameter into the corresponding ShortString[]
+    /// with the choices
+    /// @param value the encoded value
+    /// @param availableChoices the encoded available choices for the parameter, defined in parameter
+    /// definition
     function _decodeMultipleChoice(
         bytes memory value,
         bytes memory availableChoices
@@ -130,6 +179,10 @@ library Licensing {
         return result;
     }
 
+    /// @notice Sets the indexes for the chosen options in a MultipleChoice parameter,
+    /// and encodes it into bytes
+    /// @param choiceIndexes_ the indexes of the chosen options
+    /// @return the encoded value
     function _encodeMultipleChoice(
         uint8[] memory choiceIndexes_
     ) internal pure returns (bytes memory value) {
@@ -137,6 +190,12 @@ library Licensing {
         return abi.encode(mask);
     }
 
+    /// @notice Validates the value for a parameter, according to its type, meaning it will
+    /// decode the value and try to revert if it's not valid.
+    /// @dev WARNING: Some validations are not possible to do on chain (like string validation),
+    /// so they should be done off chain. Also, Boolean decoded as a Number will be valid.
+    /// @param paramDef_ the parameter definition
+    /// @param value_ the encoded value
     function _validateParamValue(
         ParamDefinition memory paramDef_,
         bytes memory value_
@@ -182,6 +241,10 @@ library Licensing {
         return true;
     }
 
+    /// @notice converts a ShortString[] to a string representation of a JSON array. Used for LicenseRegistry
+    /// metadata rendering.
+    /// @param ss the ShortString[]
+    /// @return the string representation of a JSON array
     function _shortStringArrayToJsonArray(ShortString[] memory ss) internal pure returns (string memory) {
         string memory result = "[";
         uint256 len = ss.length;
@@ -196,6 +259,11 @@ library Licensing {
         return string(abi.encodePacked(result, "]"));
     }
 
+    /// @notice decodes value from a parameter into a string representation that can be used
+    /// in LicenseRegistry metadata rendering.
+    /// @param paramDef_ the parameter definition
+    /// @param value_ the encoded value
+    /// @return the string representation of the value
     function _getDecodedParamString(Licensing.ParamDefinition memory paramDef_, bytes memory value_) internal pure returns (string memory) {
         if (paramDef_.paramType == Licensing.ParameterType.Bool) {
             return abi.decode(value_, (bool)) ? "true" : "false";
