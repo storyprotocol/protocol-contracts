fb256f1f3a0270aee92d4e502c4acec91a4badfb
Ramarti <raul@storyprotocol.xyz>
2023-12-08 02:22:48 -0300

    Fix events (#237)
    * fix emission for ipOrg config
    
    * deployed fix events
    
    ---------
    
    Co-authored-by: Raul <raul@proofofcode.dev>

--- contracts/modules/licensing/LicensingModule.sol
+++ contracts/modules/licensing/LicensingModule.sol
@@ -410,6 +410,12 @@ contract LicensingModule is BaseModule, ILicensingModule {
             ipOrgAddress
         ];
         uint256 numParams = configParams.length;
+        emit IpOrgLicensingFrameworkSet(
+            ipOrgAddress,
+            config.frameworkId,
+            LICENSING_FRAMEWORK_REPO.getLicenseTextUrl(config.frameworkId),
+            config.licensor
+        );
         // Add the parameters to storage
         for (uint256 i = 0; i < numParams; i++) {
             Licensing.ParamValue memory param = configParams[i];
@@ -422,14 +428,9 @@ contract LicensingModule is BaseModule, ILicensingModule {
                 revert Errors.LicensingModule_InvalidParamValue();
             }
             paramValues[param.tag] = param.value;
-            emit ParameterSet(ipOrgAddress, param.tag.toString(), param.value);
+            emit ParameterSet(ipOrgAddress, param.tag.toString(), paramDef.paramType, param.value);
         }
-        emit IpOrgLicensingFrameworkSet(
-            ipOrgAddress,
-            config.frameworkId,
-            LICENSING_FRAMEWORK_REPO.getLicenseTextUrl(config.frameworkId),
-            config.licensor
-        );
+        
         return "";
     }
 
