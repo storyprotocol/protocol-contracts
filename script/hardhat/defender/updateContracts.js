3bdd160983056c8d40b98ba56394c6fed4cbc3f0
Raul <raul@proofofcode.dev>
2023-12-15 15:19:02 -0500

    intalled defender

--- script/hardhat/defender/updateContracts.js
+++ script/hardhat/defender/updateContracts.js
@@ -1,11 +1,14 @@
 const loadDeployment = require('../loadDeployment.js');
+import { Defender } from '@openzeppelin/defender-sdk';
+const creds = { apiKey: process.env.DEFENDER_API_KEY, apiSecret: process.env.DEFENDER_API_SECRET };
 
 
 async function main(args, hre) {
     const { ethers } = hre;
     const { chainId, contracts } = await loadDeployment(hre);
-    
-    
+    const client = new Defender(creds);
+
+
 }
 
 module.exports = main;
